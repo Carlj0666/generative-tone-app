ac160774337351990df51c483511314e93874a7a
Carlj0666 <carlj1881@yahoo.com>
2021-11-08 12:05:47 -0800

    Adding FM synth, playing with loop and transport

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 import * as Tone from 'tone';
 
-const memSynth = new Tone.MembraneSynth().toMaster();
+const memSynth = new Tone.MembraneSynth().toDestination();
 const now = Tone.now()
+// setInterval(() => console.log(Tone.now()), 100);
+
+const synthA = new Tone.FMSynth().toDestination();
+const synthB = new Tone.FMSynth().toDestination();
+
+const loopA = new Tone.Loop(time => {
+  synthA.triggerAttackRelease("C2", "8n", time);
+}, "4n").start(0);
 
 function playSynth() {
-  memSynth.triggerAttackRelease("C2","9n", now);
-  memSynth.triggerAttackRelease("C4","9n", now + 0.5);
-  memSynth.triggerAttackRelease("C6","9n", now + 1);
+
+  document.querySelector('button')?.addEventListener('click', async () => {
+    await Tone.start()
+    console.log('audio is ready')
+  })
+  memSynth.triggerAttackRelease("C2","9n");
+  // memSynth.triggerAttackRelease("C4","9t");
+  // memSynth.triggerAttackRelease("C6","9n");
+
+
 }
 
 function App() {
+
   return (
     <div id='wrapper'>
-      <button id='button' onClick={playSynth}>Click Me</button>
+      <button id='button' onClick={playSynth}>MembraneS</button>
     </div>
   );
 }
