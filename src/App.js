7a60a3609da75acf24d624cf422477ac58e0260c
Carlj0666 <carlj1881@yahoo.com>
2021-11-12 16:49:42 -0800

    backup save

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import * as Tone from 'tone';
 const memSynth = new Tone.MembraneSynth().toDestination();
 const now = Tone.now()
 // setInterval(() => console.log(Tone.now()), 100);
-const distortion = new Tone.Distortion(1).toDestination();
+const distortion = new Tone.Distortion(0).toDestination();
 memSynth.connect(distortion)
 
 const synthA = new Tone.FMSynth().toDestination();
 const synthB = new Tone.FMSynth().toDestination();
 
 const loopA = new Tone.Loop(time => {
-  synthA.triggerAttackRelease("C2", "8n", time);
+  memSynth.triggerAttackRelease("C3", "1n", time);
 }, "4n").start(0);
 
 const loopB = new Tone.Loop(time => {
@@ -20,12 +20,12 @@ const loopB = new Tone.Loop(time => {
 
 
 function playSynth() {
-  // Tone.Transport.start()
+  Tone.Transport.start()
   document.querySelector('button')?.addEventListener('click', async () => {
     await Tone.start()
     console.log('audio is ready')
   })
-  memSynth.triggerAttackRelease("C2","9n");
+  // memSynth.triggerAttackRelease("C2","9n");
   // memSynth.triggerAttackRelease("C4","9t");
   // memSynth.triggerAttackRelease("C6","9n");
 
